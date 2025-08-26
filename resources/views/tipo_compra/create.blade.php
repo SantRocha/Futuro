b365f623c33b9bb29b9e11382f94e97c6dd19ba7
SantRocha <santiagomelo121@gmail.com>
2025-08-26 14:50:46 -0500

    Correção de nomes categoria

--- resources/views/tipo_compra/create.blade.php
+++ resources/views/tipo_compra/create.blade.php
@@ -5,7 +5,7 @@
                 <svg class="w-8 h-8 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z"></path>
                 </svg>
-                Criar Tipo de Compra
+                Criar Categoria
             </h2>
             <p class="text-emerald-100 mt-2">Adicione uma nova categoria para organizar suas compras</p>
         </div>
